25e6218ef7d7084561f8d085934d9a3bbd4d400c
ivanov kir <smyrfik1488.lera@gmail.com>
2026-09-13 21:48:18 +0300

    Add files via upload

---
+++ homework-1/create_tables.sql
@@ -0,0 +1,32 @@
+-- SQL-команды для создания таблиц
+1
+CREATE TABLE customer
+(
+	customer_id varchar(1000) PRIMARY KEY,
+    company_name varchar(1000) NOT NULL,
+    contact_name varchar(1000) NOT NULL
+);
+select * from customer
+
+2 
+CREATE TABLE employee
+(
+	employee_id int PRIMARY KEY,
+    first_name varchar(1000) NOT NULL,
+    last_name varchar(1000) NOT NULL,
+    title varchar(1000) NOT NULL,
+	birth_date varchar(1000) NOT NULL,
+	notes varchar(1000) NOT NULL
+);
+select * from employee
+
+3 
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+    customer_id varchar(1000) NOT NULL,
+    employee_id varchar(1000) NOT NULL,
+	order_date varchar(1000) NOT NULL,
+	ship_city varchar(1000) NOT NULL
+);
+select * from orders
